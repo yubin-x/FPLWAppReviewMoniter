6eb51329b98f6d87848fb64fd6125efbfd503808
WenslowZhu <wenslow.vip@gmail.com>
2019-01-06 22:29:39 +0800

    Finish A macOS App to Prevent Screen Sleep

---
+++ ReviewHelper/ReviewHelper/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ReviewHelper
+//
+//  Created by Wenslow on 2019/1/6.
+//  Copyright © 2019 Wenslow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+}
+
