1c800d3dce096860a1ed9713439b9c9c1e79d9d9
WenslowZhu <wenslow.vip@gmail.com>
2019-01-09 22:42:58 +0800

    Fix wake screen bug

--- ReviewHelper/ReviewHelper/ViewController.swift
+++ ReviewHelper/ReviewHelper/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: NSViewController {
 
     func registerSleepNotification() {
         NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.screensDidSleepNotification, object: nil, queue: OperationQueue.main) { [unowned self] (notification) in
-            if self.checkWorkingHour() {
+            if self.shouldWakeScreen() {
                 self.wakeScreen()
             }
         }
@@ -34,38 +34,12 @@ class ViewController: NSViewController {
         IOPMAssertionDeclareUserActivity(assertionName, kIOPMUserActiveLocal, &assertionID);
     }
     
-    func checkWorkingHour() -> Bool {
+    func shouldWakeScreen() -> Bool {
         let date = Date()
         let calendar = Calendar.current
         let comp = calendar.dateComponents([.hour], from: date)
-        guard let hour = comp.hour else { return false }
-        
-        let locale = Locale.current
-        let formatter = DateFormatter.dateFormat(fromTemplate: "j", options: 0, locale: locale)
-        
-        if formatter?.contains("a") == true {
-            // 12 小时制
-            return twelveHourCondition(date: date, hour: hour)
-        } else {
-            // 24 小时制
-            return twentyFourHourCondition(hour: hour)
-        }
-    }
-    
-    func twelveHourCondition(date: Date, hour: Int) -> Bool {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "a"
-        let currentAMPMFormat = dateFormatter.string(from: date).uppercased()
-        
-        if currentAMPMFormat == "AM" {
-            return hour >= 9
-        } else {
-            return hour <= 6
-        }
-    }
-    
-    func twentyFourHourCondition(hour: Int) -> Bool {
-        guard (9...18).contains(hour) else { return false }
+        guard let hour = comp.hour,
+            (9...18).contains(hour) else { return false }
         return true
     }
 }
